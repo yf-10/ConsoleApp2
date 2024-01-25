5d4bb41d8617b7d15195c8344120bb6315a7d96b
Y.Fujino <yusuke.fujino.10@gmail.com>
2024-01-25 22:00:03 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+
+
+namespace ConsoleApp2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            test();
+
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile(path: "appsettings.json");
+            //var configuration = builder.Build();
+
+            //string? str = configuration["LOG:LOGDIR_PATH"];
+            //Console.WriteLine(str);
+
+            var app = ConfigurationManager.AppSettings["test"];
+            Console.WriteLine(app);
+
+        }
+
+        private static void test()
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
